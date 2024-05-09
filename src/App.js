a8adcfba616be7ce2ebedbc0db246e7b03b67157
Rudviq <rudviq.bhavsar02@gmail.com>
2024-05-09 18:45:53 -0500

    .

--- src/App.js
+++ src/App.js
@@ -389,15 +389,11 @@ My goal is to bring innovation to problem solving methods and develop solutions 
       </div>
       <div className="right-side">
         
-        {isDarkMode ? (
+        
           <div className="logo-footer">
             <img src="./RBLogo.png" alt="RB" />
           </div>
-        ) : (
-          <div className="logo-footer-light">
-            <img src="./RBLogo_light.png" alt="RB" />
-          </div>
-        )}
+        
         <div className='footer_img'>
           <img src="insta.svg" alt="Insta" />
           <img src="linkedin_color.svg" alt="Linkedin" />
